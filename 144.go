3b59df43e60a6b81a78eca23bf92b941f4020587
Kuroome <dethe3255@gmail.com>
2026-09-18 12:01:03 +0800

    2026-09-18

---
+++ 144.go
@@ -0,0 +1,55 @@
+package leetcodelearn
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func preorderTraversal(root *TreeNode) []int {
+	result := []int{}
+	var fn func(node *TreeNode)
+	fn = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		result = append(result, node.Val)
+		fn(node.Left)
+		fn(node.Right)
+	}
+	fn(root)
+	return result
+}
+
+
+func inorderTraversal(root *TreeNode) []int {
+	result := []int{}
+	var fn func(node *TreeNode)
+	fn = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		fn(node.Left)
+		result = append(result, node.Val)
+		fn(node.Right)
+	}
+	fn(root)
+	return result
+}
+
+func postorderTraversal(root *TreeNode) []int {
+	result := []int{}
+	var fn func(node *TreeNode)
+	fn = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		fn(node.Left)
+		fn(node.Right)
+		result = append(result, node.Val)
+	}
+	fn(root)
+	return result
+}
+
+
--- 155.最小栈.go
+++ 155.最小栈.go
@@ -2,22 +2,50 @@ package leetcodelearn
 
 type MinStack struct {
 	stack []int
+	min []int
+}
+
+func Constructor() MinStack {
+    return MinStack{
+        stack: []int{},
+        min:   []int{},
+    }
 }
 
 func (this *MinStack) Push(value int) {
+	this.stack = append(this.stack, value)
+	if len(this.min) == 0 || value <= this.min[len(this.min)-1] {
+		this.min = append(this.min, value)
+	}
 
 }
 
 func (this *MinStack) Pop() {
-
+	if len(this.stack) == 0{
+		return
+	}
+	if len(this.min) == 0 {
+		return
+	}
+	pop := this.stack[len(this.stack)-1]
+	if pop == this.min[len(this.min)-1] {
+		this.min = this.min[:len(this.min)-1]
+	}
+	this.stack = this.stack[:len(this.stack)-1]
 }
 
 func (this *MinStack) Top() int {
-
+	if len(this.stack) == 0 {
+		return 0
+	}
+	return this.stack[len(this.stack)-1]
 }
 
 func (this *MinStack) GetMin() int {
-
+	if len(this.min) == 0 {
+		return 0
+	}
+	return this.min[len(this.min)-1]
 }
 
 /**
